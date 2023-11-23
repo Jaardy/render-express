58393bb3a2f5e875b080e4e61e9fcb2317562e87
Jordan McNamara <jordan.mcnamara@gmail.com>
2023-11-23 15:53:55 +0000

    attemped fix for serving static

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+console.log(path.resolve(__dirname, "../dist"));
+console.log(path.join(__dirname, "../dist"));
+// console.log(path.join(__dirname.slice(-4), "../dist"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-console.log(path.join(__dirname, "../dist"));
 // serve up static files (e.g. html and css files)
-app.use("/test", express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 module.exports = app;
