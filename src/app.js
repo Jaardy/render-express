118fc96523a94fd5eb849940b19cc24f351f93d9
Jordan McNamara <jordan.mcnamara@gmail.com>
2023-11-23 15:17:19 +0000

    express server with static dir

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// serve up static files (e.g. html and css files)
+app.use(express.static(path.join(__dirname, "../dist")));
+
+module.exports = app;
